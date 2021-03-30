9ec47c632e26e900fb0098f6f8e8c39c91035514
karm3104 <stk3143w@outlook.jp>
2021-03-30 16:58:22 +0900

    3/30学習分

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,50 +1,36 @@
-import React, { Component } from "react"
+import React, { Component } from 'react'
 import './App.css'
 
-class App extends Component{
-  data = []
+let data = {
+  title:"React-Context",
+  message:"this is sample message."
+}
 
-  area = {
-    width:"500px",
-    height:"500px",
-    border:"1px solid blue"
-  }
+const SampleContext = React.createContext(data)
 
-  constructor(props){
-    super(props)
-    this.state = {
-      list:this.data
-    }
-    this.doAction = this.doAction.bind(this)
-  }
-  doAction(e){
-    let x = e.pageX
-    let y = e.pageY
-    this.data.push({x:x,y:y})
-    this.setState({
-      list:this.data
-    })
-  }
-  draw(d){
-    let s={
-    position:"absolute",
-      left:(d.x -25) + "px",
-      top:(d.y -25) + "px",
-      width:"50px",
-      height:"50px",backgroundColor:"#66f3",
-    }
-    return <div style={s}></div>
-  }
+class App extends Component {
   render(){
-    return  <div>
-      <h1 className="bg-primary text-white display-4">React</h1>
-      <div className="container">
-        <p className="subtitle">draw rectangle.</p>
-        <div style={this.area} onClick={this.doAction}>
-          {this.data.map((value)=>this.draw(value))}
-        </div>
+    return<div>
+      <h1 className ="bg-primary text-white display-4">
+        React
+      </h1>
+      <div className = "container">
+      <Title />
+      <Message />
       </div>
     </div>
   }
 }
-export default App;
+
+class Title extends Component {
+  static contextType = SampleContext
+
+  render() {
+    return (
+      <div className = "card p-2 my-3">
+        <h2> {this.context.title} </h2>
+      </div>
+    )
+  }
+}
+export default App
